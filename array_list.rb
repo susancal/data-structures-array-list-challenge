2bf9e43aad7d0ef977d69cfc06a2d0fb1956ff50
Susie O'Brien <email": null>
2016-09-02 15:41:43 -0500

    Add set and get

--- array_list.rb
+++ array_list.rb
@@ -17,14 +17,28 @@ class ArrayList
 	end
 
 	def get(index)
-
+		if index <= (@arr.length-1)
+      return @arr[index]
+    else
+      raise OutOfBoundException, "You cant do that"
+    end
 	end
 
 	def set(index, element)
+		 if index <= (@arr.length-1)
+      @arr[index] = element
+    else
+      raise OutOfBoundException, "You cant do that"
+    end
+	end
 
+	def length
+		@arr.length
 	end
 
 end
 
 al = ArrayList.new(3)
+p al
+p al.add(4)
 p al
